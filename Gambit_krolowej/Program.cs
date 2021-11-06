472aade0f785b487942c8a2e2b6dae05194a7f5e
Marcin 119299 <pzx99984@student.wsb.poznan.pl>
2021-11-06 17:16:47 +0100

    Dodaj pliki projektów.

---
+++ Gambit_krolowej/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gambit_krolowej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
